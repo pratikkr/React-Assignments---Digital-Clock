1470ba9ab7062f11d8d52aed109ae600201d383b
pratik kumar <kumarpratik861@gmail.com>
2020-11-02 16:30:40 +0530

    changed

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,45 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    render() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: new Date().toLocaleTimeString("en-us", {
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric",
+        hour12: true
+      })
+    };
+  }
+  tick() {
+    this.setState({
+      time: new Date().toLocaleTimeString("en-us", {
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric",
+        hour12: true
+      })
+    });
+  }
 
-        return(
-            <>
-               
-            </>
-        )
-    }
+  componentDidMount() {
+    this.intervalID = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnMount() {
+    clearInterval(this.intervalID);
+  }
+  render() {
+    return (
+      <div className="App">
+        <div className="clock">
+          <h3 id="time">{`${this.state.time}`}</h3>
+        </div>
+      </div>
+    );
+  }
 }
-
 
 export default App;
